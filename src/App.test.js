1990602aea16e391c67225507b8d3f54c86c9264
Joshua Stupplebeen <jpstup@gmail.com>
2018-10-26 22:40:13 -0600

    broke something while working on tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import { render } from "react-testing-library";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
-test("it works", () => {});
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
