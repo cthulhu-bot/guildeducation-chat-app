061184606d1f6ccb6ab48e1e232959233b2466a3
Joshua Stupplebeen <jpstup@gmail.com>
2018-10-28 13:29:48 -0600

    Updates

---
+++ precache-manifest.571567c7d66c9addde11eb07e42d9e23.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b1ca3ca93f80c2cbfcfa",
+    "url": "/guildeducation-chat-app/static/js/runtime~main.b1ca3ca9.js"
+  },
+  {
+    "revision": "af7b9ae4fca5bac9a98e",
+    "url": "/guildeducation-chat-app/static/js/main.af7b9ae4.chunk.js"
+  },
+  {
+    "revision": "593a733d6628bcbedb37",
+    "url": "/guildeducation-chat-app/static/js/1.593a733d.chunk.js"
+  },
+  {
+    "revision": "af7b9ae4fca5bac9a98e",
+    "url": "/guildeducation-chat-app/static/css/main.49743e19.chunk.css"
+  },
+  {
+    "revision": "13fc09347902ac347228fb1c41f33d49",
+    "url": "/guildeducation-chat-app/index.html"
+  }
+];
